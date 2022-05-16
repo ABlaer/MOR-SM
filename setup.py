2cbc405796d0e0f129cd5e1108d31b4e3d42321f
almogblaer <blaer@post.bgu.ac.il>
2022-05-16 18:17:05 +0300

    usage ver. 2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
     
 install_requires = [
-    'pandas',
     'numpy',
     'matplotlib.pyplot',
     'os',
@@ -13,13 +12,12 @@ install_requires = [
     'glob'
     'math'
     'logging',
-    'mpl_toolkits.mplot3d.axes3d'
     ]
     
     
 setuptools.setup(
     name="MOR-SM",
-    version="1.0.0",
+    version="2.0.0",
     author="almog_blaer",
     author_email="blaer@post.bgu.ac.il",
     description="moment-rate oriented slip distribution for SW4 Seismic Waves simulation",
